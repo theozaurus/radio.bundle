aa8f2b5ea5beeceaf4b18481b5e2c549789acd5a
Theo Cushion <theo@ignition.works>
2016-04-18 18:02:05 +0100

    Add support for pls and hls playlists

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,8 +2,12 @@ ICON = 'icon-default.png'
 NAME = 'Radio'
 PREFIX = '/music/radio'
 
+CONTAINERS = {'mp4': Container.MP4, 'mp3': Container.MP3}
+CODECS = {'aac': AudioCodec.AAC, 'mp3': AudioCodec.MP3}
+RE_FILE = Regex('File1=(https?://.+)')
+
 def Start():
-    Plugin.AddViewGroup('Main Menu', viewMode = 'List', mediaType = 'items')
+    Plugin.AddViewGroup('Main Menu', viewMode='List', mediaType='items')
 
     ObjectContainer.title1 = NAME
     ObjectContainer.view_group = 'Main Menu'
@@ -14,15 +18,18 @@ def Start():
 def MainMenu():
     oc = ObjectContainer()
     for station in Dict['stations']:
-        oc.add(CreateTrackObject(station=station))
+        if station['hls']:
+            oc.add(CreateHlsTrackObject(station=station))
+        else:
+            oc.add(CreateTrackObject(station=station))
     return oc
 
-def CreateTrackObject(station, include_container=False):
+def CreateHlsTrackObject(station, include_container=False):
     track_object = TrackObject(
-        key = Callback(CreateTrackObject, station=station, include_container=True),
-        rating_key = station['url'],
-        title = station['title'],
-        items = [
+        key=Callback(CreateHlsTrackObject, station=station, include_container=True),
+        rating_key=station['url'],
+        title=station['title'],
+        items=[
             MediaObject(
                 optimized_for_streaming=True,
                 parts=[
@@ -39,3 +46,41 @@ def CreateTrackObject(station, include_container=False):
     else:
         return track_object
 
+def CreateTrackObject(station, include_container=False):
+    container = CONTAINERS[station['container']]
+    audio_codec = CODECS[station['codec']]
+
+    track_object = TrackObject(
+        key=Callback(CreateTrackObject, station=station, include_container=True),
+        rating_key=station['url'],
+        title=station['title'],
+        items=[
+            MediaObject(
+                optimized_for_streaming=True,
+                parts=[
+                    PartObject(key=Callback(PlayAudio, url=station['url'], ext=station['codec']))
+                ],
+                container=container,
+                audio_codec=audio_codec,
+            )
+        ]
+    )
+
+    if include_container:
+        return ObjectContainer(objects=[track_object])
+    else:
+        return track_object
+
+def PlayAudio(url):
+    content = HTTP.Request(url, cacheTime=0).content
+    file_url = RE_FILE.search(content)
+
+    if file_url:
+        stream_url = file_url.group(1)
+        if stream_url[-1] == '/':
+            stream_url += ';'
+        else:
+            stream_url += '/;'
+        return Redirect(stream_url)
+    else:
+        raise Ex.MediaNotAvailable
